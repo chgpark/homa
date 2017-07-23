42541a7f5becafd64767f90c4173f14f2c92bb86
chgpark <tytytr2@gmail.com>
2017-07-23 17:07:29 +0900

    23_07_17

--- octave/exercise/ex1/machine-learning-ex1/ex1/computeCost.m
+++ octave/exercise/ex1/machine-learning-ex1/ex1/computeCost.m
@@ -12,9 +12,10 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
-
-
+for i = 1:m,
+	J = J + (theta' * X(i, :)' - y(i)) ^ 2;
+end;
+J = J / (2 * m);
 
 
 % =========================================================================
